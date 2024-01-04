20057d2893c5f8229aa2f7e12865c384ad756d93
agustinleonardi <agustinleonardi5@gmail.com>
2024-01-04 11:04:25 -0300

    first setup

---
+++ WebAppSpringBoot/src/main/java/com/agustin/curso/springboot/webapp/WebAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.agustin.curso.springboot.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppSpringBootApplication.class, args);
+    }
+
+}
---
+++ WebAppSpringBoot/src/test/java/com/agustin/curso/springboot/webapp/WebAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustin.curso.springboot.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
